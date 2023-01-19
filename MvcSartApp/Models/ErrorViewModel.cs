599b8fccdde05d597085318480e775080cc16d5e
SDozH <soldatov_alex@bk.ru>
2023-01-19 20:26:16 +0300

    Добавьте файлы проекта.

---
+++ MvcSartApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcSartApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
